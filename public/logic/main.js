8457cd3d5a2b7633585753da022e8dd33605f0c8
oLabs <jonny.olabs@gmail.com>
2024-02-21 16:48:47 -0500

    updated style on ar-place-markers

--- public/logic/main.js
+++ public/logic/main.js
@@ -1,1 +1,20 @@
-console.log('hello space')
+const
+    allMarkers = document.querySelectorAll('.ar-place-marker-container')
+
+allMarkers.forEach(marker => {
+    console.log(marker)
+    const
+        markerLat = marker.querySelector('.lat'),
+        markerLng = marker.querySelector('.lng'),
+        lat = markerLat.getAttribute('data-point'),
+        lng = markerLng.getAttribute('data-point')
+
+    const markerLocation = {
+        lat: Number(lat),
+        lng: Number(lng)
+    }
+    console.log(markerLocation)
+
+    
+})
+
